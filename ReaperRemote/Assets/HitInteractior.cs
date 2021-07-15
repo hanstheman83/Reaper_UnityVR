e37c29e96c8a65d3171c3a86f536fd24324fade1
hanstheman83 <hey2k_bad@hushmail.com>
2021-07-15 16:31:29 +0200

    Project Setup - including extOSC[MIT License]

---
+++ ReaperRemote/Assets/HitInteractior.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using extOSC.Examples;
+
+public class HitInteractior : MonoBehaviour
+{
+    [SerializeField] private SimpleMessageTransmitter transmitter;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision){
+        Debug.Log("Collision");
+
+    }
+
+    private void OnTriggerEnter(Collider other){
+        Debug.Log("Triggered!");
+        GetComponent<Renderer>().material.color = Color.red;
+        // start note
+        transmitter.TransmitMidi(true, 80);
+
+    }
+    private void OnTriggerExit(Collider other){
+        Debug.Log("Triggered Exiting!");
+        GetComponent<Renderer>().material.color = Color.blue;
+        transmitter.TransmitMidi(false, 80);
+    }
+
+    
+}
