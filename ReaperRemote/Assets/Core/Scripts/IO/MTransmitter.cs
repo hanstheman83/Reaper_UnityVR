92cea6a69a4377c3bc02f323d066c7f9682b7b44
hanstheman83 <hey2k_bad@hushmail.com>
2021-07-24 20:07:30 +0200

    Finished basic movement system

---
+++ ReaperRemote/Assets/Core/Scripts/IO/MTransmitter.cs
@@ -0,0 +1,43 @@
+﻿/* Copyright (c) 2020 ExT (V.Sigalkin) */
+
+using UnityEngine;
+using extOSC;
+
+namespace Core.IO // todo check that this namespace has access to Core; ??
+{
+	public class MTransmitter : MonoBehaviour
+	{
+		#region Public Vars
+
+		public string Address = "/example/1";
+
+		[Header("OSC Settings")]
+		public OSCTransmitter Transmitter;
+
+		#endregion
+
+		#region Unity Methods
+
+		protected virtual void Start()
+		{
+			var message = new OSCMessage(Address);
+			message.AddValue(OSCValue.String("Hello, world!"));
+
+			Transmitter.Send(message);
+		}
+
+		#endregion
+
+		#region Public Methods
+		public void TransmitMidi(bool state, int note){
+			// string midiNoteMessage = "/vkb_midi/0/note/80";
+			string midiNoteMessage = $"/vkb_midi/0/note/{note}";
+			var message = new OSCMessage(midiNoteMessage);
+			message.AddValue(OSCValue.Int(state ? 1 : 0));
+			// message.AddValue(OSCValue.Int(1));
+			// Transmitter.SendMessage(message);
+			Transmitter.Send(message);
+		}
+		#endregion Public Methods
+	}
+}
---
+++ ReaperRemote/Assets/Core/Scripts/ScriptableObjects/Harp.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Harp", menuName = "Instruments/harp")]
+public class Harp : ScriptableObject
+{
+    public int firstNote, lastNote;
+    public float distanceBetweenStrings;
+}
