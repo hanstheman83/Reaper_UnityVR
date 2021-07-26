8b283d637efc8a524cbcfdc2b4ef0a4be4e060ee
hanstheman83 <hey2k_bad@hushmail.com>
2021-07-26 07:14:41 +0200

    Base Scriptable Object Instrument..

--- ReaperRemote/Assets/Core/Scripts/ScriptableObjects/Harp.cs
+++ ReaperRemote/Assets/Core/Scripts/ScriptableObjects/Harp.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "Harp", menuName = "Instruments/harp")]
-public class Harp : ScriptableObject
+
+
+public class Harp : Instrument
 {
     public int firstNote, lastNote;
     public float distanceBetweenStrings, stringWidth, stringLength;
--- ReaperRemote/Assets/Core/Scripts/ScriptableObjects/Instrument.cs
+++ ReaperRemote/Assets/Core/Scripts/ScriptableObjects/Instrument.cs
@@ -4,5 +4,5 @@ using UnityEngine;
 
 public class Instrument : ScriptableObject
 {
-    
+    public string instrumentName;
 }
