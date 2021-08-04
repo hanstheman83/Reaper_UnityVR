c0634a5bff8bab0350d016b253614807903ad3e5
hanstheman83 <hey2k_bad@hushmail.com>
2021-08-04 21:00:16 +0200

    wip data generation..

--- ReaperRemote/Assets/Core/Scripts/InstrumentGenerator.cs
+++ ReaperRemote/Assets/Core/Scripts/InstrumentGenerator.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using DataTransfer = Core.IO;
-using Data = Core.Data;
+using Core.Data;
 
 
 public class InstrumentGenerator : MonoBehaviour
@@ -16,7 +16,7 @@ public class InstrumentGenerator : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Data::Data.Init();
+        Data.Init();
 
         GenerateInstrument2();
 
@@ -25,7 +25,46 @@ public class InstrumentGenerator : MonoBehaviour
             GameObject newInstrument = new GameObject("new harp");
             newInstrument.transform.position = positionTransform.position;
             newInstrument.transform.SetParent(instrumentsLocation.transform);
+            // start octave
+            // # strings
+            // root note 
+            // scale
+            // 
             
+            if(!Data.RootNotes.TryGetValue(harp.rootNote, out var innerDic)){ Debug.LogError("No key found!"); }
+            if(!innerDic.TryGetValue(harp.startOctave, out int midiNote)){ Debug.LogError("No key found!"); }
+            Debug.Log("First midi note " + midiNote);
+            int j = 0;
+            if(!Data.Scales.TryGetValue(harp.scale, out int[] scale)) { Debug.LogError("No key found!"); }
+
+            for(int i = 0; i < harp.numStrings; i++){
+                // create first string plus scales[j] 
+                // iterate j : next in scales.. reset if scales.length.
+                // calc midiNote : plus j in scale.. 
+                // iterate scale : add (result -1) for correct distance
+                
+                // name GO as midiNote!
+
+                // GameObject newString = new GameObject();
+                // //newString = Instantiate(newString, Vector3.zero, Quaternion.identity);
+                // newString.transform.SetParent(newHarp.transform);
+                // newString.transform.localPosition = Vector3.zero;
+                // // displace from zero at newHarp
+                // float stringOffset = harp.stringLength/2;
+                // newString.transform.localPosition = new Vector3(i * harp.distanceBetweenStrings, stringOffset, 0);
+                // GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                // cube.transform.SetParent(newString.transform);
+                // cube.transform.localPosition = Vector3.zero;
+                // // cylinder is 2m height by default
+                // cube.transform.localScale = new Vector3(harp.stringWidth, harp.stringLength, harp.stringWidth);
+                // // 
+                // cube.GetComponent<Collider>().isTrigger = true;
+                // HitInteractor hitInteractor = newString.AddComponent<HitInteractor>();
+                // hitInteractor.MidiNote = currentString;
+                // hitInteractor.Transmitter = FindObjectOfType<DataTransfer::MTransmitter>();
+                // cube.AddComponent<ChildTrigger>();
+                // cube.layer = LayerMask.NameToLayer("MusicInteraction");
+            }
             
             
         }
