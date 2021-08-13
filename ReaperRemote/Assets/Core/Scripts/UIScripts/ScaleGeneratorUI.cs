3886654e330669994190d40a2b385bff8d176dc7
hanstheman83 <hey2k_bad@hushmail.com>
2021-08-13 17:14:34 +0200

    WIP choosing root and scale and auto generate new scale

---
+++ ReaperRemote/Assets/Core/Scripts/UIScripts/ScaleGeneratorUI.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScaleGeneratorUI : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // read from Data and generate options list in dropdown
+        // str = string.Concat(str.Select(x => Char.IsUpper(x) ? " " + x : x.ToString())).TrimStart(' ');
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
