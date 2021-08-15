578fc96798688733eddc095ba705ffb26ea4f941
hanstheman83 <hey2k_bad@hushmail.com>
2021-08-15 22:07:37 +0200

    WIP adding custom composite input actions

--- ReaperRemote/Assets/Core/Scripts/Controls/InputActionController.cs
+++ ReaperRemote/Assets/Core/Scripts/Controls/InputActionController.cs
@@ -15,6 +15,9 @@ public class InputActionController : MonoBehaviour
     public bool rightIsActive = true;
     private bool oldState = false;
 
+    //[SerializeField] private InputActionMap leftXRIMap; // setup of combinable actions
+    //[SerializeField] InputActionAsset mainAsset;
+
     [SerializeField] private ActionBasedControllerManager leftActionBasedControllerManager;
     [SerializeField] private ActionBasedControllerManager rightActionBasedControllerManager;
     [SerializeField] private UI_InteractionController leftUI_InteractionController;
@@ -35,9 +38,12 @@ public class InputActionController : MonoBehaviour
     [SerializeField]private InputActionProperty rightMove;
     [SerializeField]private InputActionProperty leftTurn;
     [SerializeField]private InputActionProperty rightTurn;
+    [SerializeField]private InputActionProperty leftTrigger;
+    [SerializeField]private InputActionProperty rightTrigger;
+
+    private InputAction combined;
 
 
-    // set of inputactions stored in scriptable objects 
     // perhaps use these as a compilation/set of controls
 
     // control components should start disactivated
@@ -46,6 +52,34 @@ public class InputActionController : MonoBehaviour
         // search for components ? or hard-link
         customMoveProvider = FindObjectOfType<CustomMoveProvider>();
         customSnapTurnProvider = FindObjectOfType<CustomSnapTurnProvider>();
+
+        #region old code
+        combined = new InputAction();
+        // Debug.Log(leftTrigger.action.bindings[0].ToString().Colorize(Color.black));
+        combined.AddCompositeBinding("Axis").
+            With("Positive", "<XRController>{RightHand}/triggerPressed").
+            With("Negative", "<XRController>{LeftHand}/triggerPressed");
+        combined.Enable();
+        //    https://docs.unity3d.com/Packages/com.unity.inputsystem@1.0/api/UnityEngine.InputSystem.InputAction.html
+        combined.started += context => Debug.Log($"{context.action} started");
+        combined.performed += context => Debug.Log($"{context.action} performed");
+        combined.canceled += context => Debug.Log($"{context.action} canceled");
+        combined.started += Test;
+        
+        // https://docs.unity3d.com/Packages/com.unity.inputsystem@1.0/manual/ActionBindings.html
+        // https://forum.unity.com/threads/trying-to-define-a-custom-composite-to-the-new-unity-input-system-but-failing-somewhere.904889/
+        // new input system still unstable!!
+
+        #endregion old code
+
+        
+    }
+    void Test(InputAction.CallbackContext obj){
+        Debug.Log("Test!".Colorize(Color.black));
+        Debug.Log("action type :" + obj.valueType);
+        // Debug.Log("action type :" + obj.v);
+        var v = obj.ReadValue<float>();
+        Debug.Log($"value {v}".Colorize(Color.green));
     }
 
     private void Start() {
