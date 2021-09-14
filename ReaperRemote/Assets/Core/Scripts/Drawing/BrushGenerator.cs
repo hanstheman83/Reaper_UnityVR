9ae50a660a8f887f8f8a932b2f063c1c04b62bae
hanstheman83 <hey2k_bad@hushmail.com>
2021-09-14 21:19:59 +0200

    WIP brush sizes generation

--- ReaperRemote/Assets/Core/Scripts/Drawing/BrushGenerator.cs
+++ ReaperRemote/Assets/Core/Scripts/Drawing/BrushGenerator.cs
@@ -14,19 +14,21 @@ public struct Brush{ // contains several arrays of diffrent sizes
     public float Softness {get => softness;}
     private float roundness;
     public float Roundness {get => Roundness;}
+    private int numberOfSizes;
     //
-    private Color[][] brushSizes; // first index = sizeArray, 2nd index(in sizeArray) = pixels in brush
-    // can add extra color arrays for "dirty" colors - procedually generated random dirt algos!
+    private float[][] brushSizesValues; // 0 - 1f transparency of brush. 0 then no pixel. 1 then 100% strength.
+    // first index = size of brush, 2nd index  = transparency of pixels for that size of brush
 
     /// <summary>
     /// Softness and Roundness values must be between 0 and 1!
     /// </summary>
-    public Brush(int height = 5, int width = 5, float softness = 0f, float roundness = 1f){ // min pixel size
+    public Brush(int height = 5, int width = 5, int numberOfSizes = 10, float softness = 0f, float roundness = 1f){ // min pixel size
         this.height = height;
         this.width = width;
         this.softness = softness;
         this.roundness = roundness;
-        this. brushSizes = new Color[0][]; // needed init in a struct!
+        this.brushSizesValues = new float[numberOfSizes][]; // needed init in a struct!
+        this.numberOfSizes = numberOfSizes;
         // Generate size arrays
         GenerateSizeArrays();
     }
@@ -36,6 +38,23 @@ public struct Brush{ // contains several arrays of diffrent sizes
         // +1 = grow 1p , so 1p wider than org
         //
 
+        // 1st version : square a x b
+        // softness : 
+        
+        // 
+        int counter;
+        for (var i = 0; i < numberOfSizes; i++)
+        {   
+            counter = 0;
+            for (var j = 0; j < height + i; j++)
+            {
+                for (var k = 0; k < width + i; k++)
+                {
+                    brushSizesValues[i][counter] = 1f;
+                    counter++;
+                }
+            }
+        }
     }
 }
 public static class BrushGenerator
