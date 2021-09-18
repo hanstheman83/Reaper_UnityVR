bb661beb98152e20dc909c38ce4b01b0c86117be
hanstheman83 <hey2k_bad@hushmail.com>
2021-09-18 15:46:19 +0200

    done layers with alpha blend

--- ReaperRemote/Assets/Core/Scripts/Drawing/LayerManager.cs
+++ ReaperRemote/Assets/Core/Scripts/Drawing/LayerManager.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-//
+// Transparency on level of Pixel : for soft brush strokes!
 
 public class LayerManager : MonoBehaviour
 {
@@ -18,9 +18,12 @@ public class LayerManager : MonoBehaviour
 
 
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
         activeLayer = layersList[0];
+        // need null pointer as last entry in list
+        layersList.Add(null);
+        if(layersList[2] == null) Debug.Log("layer is null");
 
         
         // if a on draw on active layer is 1 set thin to cl, otherwise calc color!
@@ -37,14 +40,59 @@ public class LayerManager : MonoBehaviour
         background.InitializeBackground(width, height, Colors.White);
         foreach (var layer in layersList)
         {
-            layer.InitializeLayer(width, height);
+            layer?.InitializeLayer(width, height);
         }
         combinedLayers = new Color32[width * height];
         // init combinedlayers
+        CalculateCombinedLayers(); 
+    }
+    private void CalculateCombinedLayers(){
         for (var i = 0; i < combinedLayers.Length; i++)
         {
-            combinedLayers[i] = Colors.Black;
+            // if(layersList[0].Pixels[i].a == 255){
+            //     combinedLayers[i] = layersList[0].Pixels[i];
+            // }else if(layersList[1].Pixels[i].a == 255){
+            //     combinedLayers[i] = layersList[1].Pixels[i];
+            // }
+            combinedLayers[i] = CalculateTopPixel(i, 0); // recursive, start from top layer, index = 0
         }
+    }
+
+    /// <summary>
+    /// Recursive Function. Calculate pixel at layerPosition in list. Index 0 is top layer.
+    /// </summary>
+    private Color32 CalculateTopPixel(int pixel, int layerPosition){ // recursively from top, stop if layer has pixel alpha of 255!
+        //Debug.Log("Calculating top pixel");
+        Color32 topPixel = layersList[layerPosition].Pixels[pixel]; 
+        //Debug.Log("Before ifs");
+        //Debug.Log("topPixel a" + topPixel.a);
+        if(topPixel.a == 255){
+            // topPixel has right color since this layer has alpha of 255!
+            // do nothing
+        }else if(layersList[layerPosition + 1] != null){
+            //Debug.Log("case next layer");
+            topPixel = PixelBlend(topPixel, CalculateTopPixel(pixel, (layerPosition + 1) ));
+        }else {// if no layer blend with background
+            // blend with background
+            //Debug.Log("case background");
+            topPixel = PixelBlend(topPixel, background.Pixels[pixel]);
+        }
+        return topPixel;
+    }
+
+    // returning calculated pixel for combinedLayer - not setting alpha levels of layers. 
+    // treating bottom pixel as alpha : a = 1 - topPixel.a in range 0 to 1 
+    private Color32 PixelBlend(Color32 topPixel, Color32 bottomPixel){ // top - bottom : relative to their layer
+        //Debug.Log("Pixel blending...");
+        // blend based on top layer alpha
+        // alpha is 0 to 255
+        float topPixelAlphaPercentage = (topPixel.a/255f);
+        //float bottomPixelAlphaPercentage = (bottomPixel.a/255f);
+        //Debug.Log("alpha blend : "+ topPixelAlphaPercentage);
+        return new Color32( (byte)Mathf.Clamp( ((topPixel.r * topPixelAlphaPercentage) + (bottomPixel.r * (1 - topPixelAlphaPercentage))), 0, 255 ),
+                            (byte)Mathf.Clamp( ((topPixel.g * topPixelAlphaPercentage) + (bottomPixel.g * (1 - topPixelAlphaPercentage))), 0, 255 ),
+                            (byte)Mathf.Clamp( ((topPixel.b * topPixelAlphaPercentage) + (bottomPixel.b * (1 - topPixelAlphaPercentage))), 0, 255 ),
+                            1);
     }
 
     // public void CreateLayer(int height, int width){
@@ -62,7 +110,7 @@ public class LayerManager : MonoBehaviour
         //Set it to a new colour: Color newColour = new Color(r, g, b);
         // in linear color texture! 
         // scale by alpha!
-        
+
         //
         if(color.a < 255) { // calc combined color, background + layers
             // background.pixels[n]
